5f5512b7d5a5fbae25cdda94ca924a62769fd83a
Gabriele Falace <gabrielefalace@gmail.com>
2022-06-27 01:57:50 +0200

    Create nearest_neighbor_simplified.py

---
+++ nearest_neighbor_simplified.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x_train = np.random.rand(10, 3)   # generate 10 random vectors of dimension 3
+x_test = np.random.rand(3)        # generate one more random vector of the same dimension
+
+def dist(a, b):
+    sum = 0
+    for ai, bi in zip(a, b):
+        sum = sum + (ai - bi)**2
+    return np.sqrt(sum)
+    
+def nearest(x_train, x_test):
+    nearest = -1
+    min_distance = np.Inf
+    for i, vect in enumerate(x_train):
+        current_dist = dist(vect, x_test)
+        if current_dist < min_distance:
+            min_distance = current_dist
+            nearest = i
+    print(nearest)
+
+nearest(x_train, x_test)
