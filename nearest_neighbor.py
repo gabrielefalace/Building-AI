42058df8cd8c63d21063576885433c8505efb3a3
Gabriele Falace <gabrielefalace@gmail.com>
2022-06-27 13:56:12 +0200

    Create nearest_neighbor.py

---
+++ nearest_neighbor.py
@@ -0,0 +1,74 @@
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+
+# create random data with two classes
+X, Y = make_blobs(n_samples=16, n_features=2, centers=2, center_box=(-2, 2))
+
+# scale the data so that all values are between 0.0 and 1.0
+X = MinMaxScaler().fit_transform(X)
+
+# split two data points from the data as test data and
+# use the remaining n-2 points as the training data
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=2)
+
+# place-holder for the predicted classes
+y_predict = np.empty(len(y_test), dtype=np.int64)
+
+# produce line segments that connect the test data points
+# to the nearest neighbors for drawing the chart
+lines = []
+
+# distance function
+def dist(a, b):
+    sum = 0
+    for ai, bi in zip(a, b):
+        sum = sum + (ai - bi)**2
+    return np.sqrt(sum)
+
+def find_3_smallest(a):
+    indexed = []
+    for i, elem in enumerate(a):
+        pair = (i, elem)
+        indexed.append(pair)
+    #print(f"Sorted: {indexed}")
+    indexed.sort(key=lambda t: t[1])
+    return list(map(lambda t: t[0], indexed[:3]))
+
+def main(X_train, X_test, y_train, y_test):
+
+    global y_predict
+    global lines
+
+    k = 3    # classify our test items based on the classes of 3 nearest neighbors
+
+    # process each of the test data points
+    for i, test_item in enumerate(X_test):
+        # calculate the distances to all training points
+        distances = [dist(train_item, test_item) for train_item in X_train]
+
+        # add your code here
+
+        k3_nearest = find_3_smallest(distances)
+
+        #print(f"K3 Nearest: {k3_nearest}")
+
+        #nearest = np.argmin(distances)       # this just finds the nearest neighbour (so k=1)
+        
+
+        # create a line connecting the points for the chart
+        # you may change this to do the same for all the k nearest neigbhors if you like
+        # but it will not be checked in the tests
+        #lines.append(np.stack((test_item, X_train[nearest])))
+
+        predictions_nearest = []
+        for j in k3_nearest:
+            predictions_nearest.append(y_train[j])
+
+        y_predict[i] = 0 if sum(predictions_nearest) <=1 else 1         # this just classifies everything as 0
+    
+    print(y_predict)
+
+main(X_train, X_test, y_train, y_test)
